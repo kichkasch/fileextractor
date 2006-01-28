2deb2e34e116e982c0a0f60360a682deb19c2bb4
Michael Pilgermann <kichkasch@gmx.de>
2006-01-28 23:19:10 +0000

    create win executables

---
+++ setup_win.py
@@ -0,0 +1,18 @@
+"""
+setup_win.py
+
+Setup file for Windows32 - creating an executable from all the Python sources
+
+Don't forget to put the icons, the help zip and the dd  clone afterwards.
+
+@author: Michael Pilgermann
+@contact: mailto:mpilgerm@glam.ac.uk
+@organization: Information Security Group / University of Glamorgan 
+@contact: http://www.glam.ac.uk/soc/research/isrg.php
+@license: GPL (General Public License)
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(windows=["fileextractor.py"])
